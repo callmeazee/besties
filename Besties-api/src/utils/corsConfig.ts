725bd7bba10175d05abd3ad36cf42e9b2f6b981a
Azeez <azeezahemedkhan@gmail.com>
2026-07-29 19:54:18 +0530

    cors error fixing

--- Besties-api/src/utils/corsConfig.ts
+++ Besties-api/src/utils/corsConfig.ts
@@ -1,6 +1,9 @@
-const corsConfig = {
-     origin: process.env.CLIENT,
-    credentials: true
+// Fix trailing slash automatically if process.env.CLIENT has one
+const clientUrl = process.env.CLIENT?.replace(/\/$/, "");
 
-}
-export default corsConfig
+const corsConfig = {
+  origin: clientUrl || "https://besties-sandy.vercel.app",
+  credentials: true,
+};
+
+export default corsConfig;
